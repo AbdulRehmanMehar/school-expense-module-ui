1c5ea5917bbba598a62709ed5f94f96de73d321e
Abdul Rehman <mehars.6925@gmail.com>
2020-01-19 11:45:51 +0500

    set route for home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Home - School Expense Module'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
