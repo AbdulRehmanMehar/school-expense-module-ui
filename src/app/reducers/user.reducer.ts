476712dbfa6540f23d01eed940cc5ef97d23cff2
Abdul Rehman <mehars.6925@gmail.com>
2020-01-20 21:09:46 +0500

    stores state into localstorage

--- src/app/reducers/user.reducer.ts
+++ src/app/reducers/user.reducer.ts
@@ -2,30 +2,46 @@ import { User } from './../models/user.model';
 import { createReducer, on } from '@ngrx/store';
 import { changeUserToPrincipal, changeUserToAccountant } from '../actions/user.actions';
 
+let oldUser: User;
+const rawUser = localStorage.getItem('user');
 
+if (rawUser) {
+  oldUser = JSON.parse(rawUser);
+} else {
+  oldUser = {
+    name: 'Accountant',
+    role: 'accountant',
+    image: 'accountant-avatar.jpeg'
+  };
+  localStorage.setItem('user', JSON.stringify(oldUser));
+}
 
-export const initialState: User =   {
-  name: 'Accountant',
-  role: 'accountant',
-  image: 'accountant-avatar.jpeg'
-};
+export const initialState: User = oldUser;
 
 const _userReducer = createReducer(initialState,
   // on(increment, state => state + 1),
   // on(decrement, state => state - 1),
   // on(reset, state => 0),
-  on(changeUserToAccountant, state =>
-    state = {
-      name: 'Accountant',
-      role: 'accountant',
-      image: 'accountant-avatar.jpeg'
+  on(changeUserToAccountant, state => {
+      const user = {
+        name: 'Accountant',
+        role: 'accountant',
+        image: 'accountant-avatar.jpeg'
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+      state = user;
+      return state;
     }
   ),
-  on(changeUserToPrincipal, state =>
-    state = {
-      name: 'Principal',
-      role: 'principal',
-      image: 'principal-avatar.jpeg'
+  on(changeUserToPrincipal, state => {
+      const user = {
+        name: 'Principal',
+        role: 'principal',
+        image: 'principal-avatar.jpeg'
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+      state = user;
+      return state;
     }
   )
 );
