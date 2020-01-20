383feb88720d1c5c7df17f00b8f070e7bf0eed14
Abdul Rehman <mehars.6925@gmail.com>
2020-01-20 13:52:20 +0500

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ChartComponent } from './chart/chart.component';
+import { BudgetLogComponent } from './budget-log/budget-log.component';
+
+import { BudgetIssuanceComponent } from './budget-issuance/budget-issuance.component';
+import { ModuleInsidesComponent } from './module-insides/module-insides.component';
+import { AddModuleComponent } from './add-module/add-module.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ChartComponent],
+  declarations: [AppComponent, HomeComponent, ChartComponent, BudgetLogComponent, BudgetIssuanceComponent, ModuleInsidesComponent, AddModuleComponent],
   imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [Title],
   bootstrap: [AppComponent]
