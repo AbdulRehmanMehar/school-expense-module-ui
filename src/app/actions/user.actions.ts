088a344c691bc31815dcc163fbb5a74e78bea754
Abdul Rehman <mehars.6925@gmail.com>
2020-01-20 20:48:25 +0500

    user action

---
+++ src/app/actions/user.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@ngrx/store';
+
+export const changeUserToPrincipal = createAction('[User Component] ChangeUserToPrincipal');
+export const changeUserToAccountant = createAction('[User Component] ChangeUserToAccountant');
