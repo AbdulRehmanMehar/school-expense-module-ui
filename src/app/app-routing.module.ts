4898e22c5a27be0af2f121466ab5a4046790350e
Abdul Rehman <mehars.6925@gmail.com>
2020-01-20 13:52:38 +0500

    added routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ModuleInsidesComponent } from './module-insides/module-insides.component';
+import { AddModuleComponent } from './add-module/add-module.component';
+import { BudgetIssuanceComponent } from './budget-issuance/budget-issuance.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { BudgetLogComponent } from './budget-log/budget-log.component';
+
 
 const routes: Routes = [
   {
@@ -10,7 +15,42 @@ const routes: Routes = [
     data: {
       title: 'Home - School Expense Module'
     }
-  }
+  },
+  {
+    path: 'budget-log',
+    component: BudgetLogComponent,
+    data: {
+      title: 'View Issued Budget - School Expense Module'
+    }
+  },
+  // {
+  //   path: 'budget-insides',
+  //   component: BudgetInsidesComponent,
+  //   data: {
+  //     title: 'View Budget Insides - School Expense Module'
+  //   }
+  // },
+  {
+    path: 'issue-budget',
+    component: BudgetIssuanceComponent,
+    data: {
+      title: 'Issue Budget - School Expense Module'
+    }
+  },
+  {
+    path: 'add-module',
+    component: AddModuleComponent,
+    data: {
+      title: 'Create Sub Module - School Expense Module'
+    }
+  },
+  {
+    path: 'view-modules',
+    component: ModuleInsidesComponent,
+    data: {
+      title: 'View Sub Modules - School Expense Module'
+    }
+  },
 ];
 
 @NgModule({
