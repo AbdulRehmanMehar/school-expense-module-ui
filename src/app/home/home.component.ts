4ca6402cc9e9b14c2b1ea2ed237c761e2378acce
Abdul Rehman <mehars.6925@gmail.com>
2020-02-13 02:41:53 +0500

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
+import { State } from './../store/models/app-state.model';
 import { Component, OnInit } from '@angular/core';
-import { User } from '../models/user.model';
+import { User } from '../store/models/user.model';
 import { Store, select } from '@ngrx/store';
+import { Budget } from '../store/models/budget.model';
+import { Module } from '../store/models/module.model';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +12,40 @@ import { Store, select } from '@ngrx/store';
 })
 export class HomeComponent implements OnInit {
   public user;
-  constructor(private store: Store<{ user: User }>) {}
+  public budget$: Budget[];
+  public modules$: Module[];
+
+
+  constructor(private store$: Store<State>) {}
 
   ngOnInit() {
-    this.store.pipe(select('user'))
+    this.loadState();
+  }
+
+  private loadState() {
+    this.store$.pipe(select('user'))
       .subscribe(user => this.user = user);
+
+    this.store$.pipe(select('module'))
+    .subscribe(mod => {
+      this.modules$ = mod.modules;
+    });
+
+    this.store$.pipe(select('budget'))
+    .subscribe(budget => {
+      this.budget$ = budget.budget;
+    });
+
+
+
+  }
+
+  getTotalBudget() {
+    let amount = 0;
+    this.budget$.forEach(budget => {
+      // tslint:disable-next-line: radix
+      amount += parseInt(budget.amount);
+    });
+    return amount;
   }
 }
