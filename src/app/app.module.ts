105107ac687262f68e9d8ad7c0f80f9e3872e428
Abdul Rehman <mehars.6925@gmail.com>
2020-02-13 02:29:11 +0500

    imports for dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { ModuleEffects } from './store/effects/module.effects';
+import { BudgetEffects } from './store/effects/budget.effects';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NotifierModule } from 'angular-notifier';
 import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,9 +14,16 @@ import { BudgetLogComponent } from './budget-log/budget-log.component';
 import { BudgetIssuanceComponent } from './budget-issuance/budget-issuance.component';
 import { ModuleInsidesComponent } from './module-insides/module-insides.component';
 import { AddModuleComponent } from './add-module/add-module.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.prod';
 
 import { StoreModule } from '@ngrx/store';
-import { userReducer } from './reducers/user.reducer';
+import { userReducer } from './store/reducers/user.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BudgetReducer } from './store/reducers/budget.reducer';
+import { ModuleReducer } from './store/reducers/module.reducer';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,7 +39,13 @@ import { userReducer } from './reducers/user.reducer';
     BrowserModule,
     AppRoutingModule,
     ChartsModule,
-    StoreModule.forRoot({ user: userReducer })
+    EffectsModule.forRoot([BudgetEffects, ModuleEffects]),
+    StoreModule.forRoot({ user: userReducer, budget: BudgetReducer, module: ModuleReducer }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NotifierModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
