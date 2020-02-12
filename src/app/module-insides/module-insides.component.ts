280baaaf28c870840618ec0c5516169404386f46
Abdul Rehman <mehars.6925@gmail.com>
2020-02-13 02:42:01 +0500

    updated

--- src/app/module-insides/module-insides.component.ts
+++ src/app/module-insides/module-insides.component.ts
@@ -1,4 +1,8 @@
+import { Budget } from './../store/models/budget.model';
+import { Store, select } from '@ngrx/store';
+import { Module } from './../store/models/module.model';
 import { Component, OnInit } from '@angular/core';
+import { State } from '../store/models/app-state.model';
 
 @Component({
   selector: 'app-module-insides',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuleInsidesComponent implements OnInit {
 
-  constructor() { }
+  budget$: Budget[];
+  modules$: Module[];
+
+  constructor(private store$: Store<State>) { }
 
   ngOnInit() {
+    this.loadState();
+  }
+
+  deleteModule(id) {
+
+  }
+
+  getBudgetOfModule(id) {
+    let amount = 0;
+    this.budget$.forEach(budget => {
+      if (budget.module == id) {
+        // tslint:disable-next-line: radix
+        amount += parseInt(budget.amount);
+      }
+    });
+    return amount;
+  }
+
+  private loadState() {
+    this.store$.pipe(select('module'))
+      .subscribe(modules => {
+        this.modules$ = modules.modules;
+      });
+
+    this.store$.pipe(select('budget'))
+      .subscribe(budget => {
+        this.budget$ = budget.budget;
+      });
   }
 
 }
