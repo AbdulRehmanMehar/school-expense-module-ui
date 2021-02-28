2fdab0c4cefeb9b80895afd5111d5dbaa3c2e833
Abdul Rehman <mehars.6925@gmail.com>
2021-02-28 15:26:16 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:  {
+    apiKey: 'AIzaSyA_jj8xS82E-0UzKyXbM8GnBRsFHXiN0m0',
+    authDomain: 'school-expense-module.firebaseapp.com',
+    databaseURL: 'https://school-expense-module.firebaseio.com',
+    projectId: 'school-expense-module',
+    storageBucket: 'school-expense-module.appspot.com',
+    messagingSenderId: '1031363210014',
+    appId: '1:1031363210014:web:6177ad5aa0fef59b10f70f',
+    measurementId: 'G-1CQCPSFNCS'
+  }
 };
 
 /*
